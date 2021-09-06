dcd4d36b126026feccac9088c02046157a6a9f17
rtao <rtao@amarsoft.com>
2021-09-06 09:54:02 +0800

    1

--- urule-springboot/src/main/java/com/bstek/urule/springboot/Application.java
+++ urule-springboot/src/main/java/com/bstek/urule/springboot/Application.java
@@ -1,8 +1,13 @@
 package com.bstek.urule.springboot;
 
+import com.bstek.urule.console.servlet.URuleServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+
+import javax.servlet.http.HttpServlet;
 
 /**
  * @author Jacky.gao
@@ -14,4 +19,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class,args);
 	}
+
+	@Bean
+	public ServletRegistrationBean<HttpServlet> registerURuleServlet(){
+		return new ServletRegistrationBean<HttpServlet>(new URuleServlet(),"/urule/*");
+	}
+
 }
--- urule-springboot/src/main/java/com/bstek/urule/springboot/action/MyMathAction.java
+++ urule-springboot/src/main/java/com/bstek/urule/springboot/action/MyMathAction.java
@@ -1,10 +1,10 @@
 package com.bstek.urule.springboot.action;
 
 import com.bstek.urule.Utils;
+import com.bstek.urule.model.ExposeAction;
 import com.bstek.urule.model.library.action.annotation.ActionBean;
 import com.bstek.urule.model.library.action.annotation.ActionMethod;
 import com.bstek.urule.model.library.action.annotation.ActionMethodParameter;
-import com.bstek.urule.runtime.builtinaction.MathAction;
 
 import java.math.BigDecimal;
 
@@ -13,8 +13,9 @@ import java.math.BigDecimal;
  * @date 2021/9/2 19:04
  */
 @ActionBean(name = "自定义数学函数")
-public class MyMathAction extends MathAction {
+public class MyMathAction {
 
+    @ExposeAction(value = "向上取整", parameters = {"数字"})
     @ActionMethod(name = "向上取整")
     @ActionMethodParameter(names = {"数字"})
     public Number ceil(Object num) {
